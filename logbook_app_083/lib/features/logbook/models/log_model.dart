6fdb378f8fb786b2a9a3352403728f662a56c358
Ihsan Ramadhan <ihsanhandsome06@gmail.com>
2026-03-03 09:53:23 +0700

    Langkah 4 & 5 bagian 4.1

--- logbook_app_083/lib/features/logbook/models/log_model.dart
+++ logbook_app_083/lib/features/logbook/models/log_model.dart
@@ -1,10 +1,14 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
 class LogModel {
+  final ObjectId? id;
   final String title;
   final String date;
   final String description;
   final String category;
 
   LogModel({
+    this.id,
     required this.title,
     required this.date,
     required this.description,
@@ -14,6 +18,7 @@ class LogModel {
   // Untuk Tugas HOTS: Konversi Map (JSON) ke Object
   factory LogModel.fromMap(Map<String, dynamic> map) {
     return LogModel(
+      id: map['_id'] as ObjectId?,
       title: map['title'],
       date: map['date'],
       description: map['description'],
@@ -23,11 +28,15 @@ class LogModel {
 
   // Konversi Object ke Map (JSON) untuk disimpan
   Map<String, dynamic> toMap() {
-    return {
+    final map = <String, dynamic>{
       'title': title,
       'date': date,
       'description': description,
       'category': category,
     };
+    if (id != null) {
+      map['_id'] = id;
+    }
+    return map;
   }
 }
